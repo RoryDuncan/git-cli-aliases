9765a2965e261ee9fda8b61b12cc6c65b2cd14ec
Rory Duncan <RoryDuncan@users.noreply.github.com>
2017-10-04 16:35:14 -0700

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+
+# adds 'git summary', showing a line diff of each file
+git config --global alias.summary 'diff HEAD~1 HEAD --stat=80,40'
+
+# adds 'git l', showing a one-line pretty print log
+git config --global alias.l 'log --abbrev-commit --pretty=oneline'
